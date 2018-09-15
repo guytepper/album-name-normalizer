aa3d3dd067d9d83ebfd106aa9e174dbd63f1e3c8
Guy Tepper <guytepper@gmail.com>
2018-09-15 16:43:04 +0300

    Added removeDescriptors boilerplate

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Remove descriptors from the provided album name.
+ * @param {string} albumName - The album name to remove descriptors from.
+ * @returns {string} The album name without descriptors.
+ */
+function removeDescriptors(albumName) {}
+
+module.exports = removeDescriptors;
