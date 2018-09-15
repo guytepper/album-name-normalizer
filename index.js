4d51f1c678d81a019c74eba9f8974e6002e31f42
guytepper <guytepper@gmail.com>
2018-09-15 17:52:17 +0300

    Make removeDescriptors function work

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
  * @param {string} albumName - The album name to remove descriptors from.
  * @returns {string} The album name without descriptors.
  */
-function removeDescriptors(albumName) {}
+function removeDescriptors(albumName) {
+  let descriptorIndex = -1;
+  descriptorIndex = albumName.indexOf('Remaster');
+
+  if (descriptorIndex < 0) {
+    descriptorIndex = albumName.indexOf('Deluxe Edition');
+  }
+
+  if (descriptorIndex < 0) {
+    descriptorIndex = albumName.indexOf('Explicit Version');
+  }
+
+  // Descriptors are usually wrapped inside brackets, so we'll remove anything inside those brackets.
+  if (descriptorIndex > 0) {
+    const lastBracket = albumName.lastIndexOf('(');
+    const normalizedName = albumName.slice(0, lastBracket - 1);
+    return normalizedName;
+  }
+  return albumName;
+}
 
 module.exports = removeDescriptors;
