3b316aa11e53010ec18cd55417b1c9da37dca1f7
guytepper <guytepper@gmail.com>
2019-05-11 13:35:26 +0300

    Changed the way descriptors are detected

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+const descriptors = ['Remaster', 'Deluxe', 'Explicit', 'Original Motion Picture Soundtrack', 'Single'];
+
 /**
  * Remove descriptors from the provided album name.
  * @param {string} albumName - The album name to remove descriptors from.
  * @returns {string} The album name without descriptors.
  */
 function removeDescriptors(albumName) {
-  let descriptorIndex = -1;
-  descriptorIndex = albumName.indexOf('Remaster');
+  const foundDescriptors = descriptors.find(d => albumName.indexOf(d) > 0) || [];
 
-  if (descriptorIndex < 0) {
-    descriptorIndex = albumName.indexOf('Deluxe');
-  }
+  if (foundDescriptors.length > 0) {
+    let normalizedName = '';
 
-  if (descriptorIndex < 0) {
-    descriptorIndex = albumName.indexOf('Explicit Version');
-  }
+    // Check if the spacing character between the album name & the descriptor is a hyphen.
+    if (albumName.charAt(albumName.indexOf(foundDescriptors[0]) - 2) === '-') {
+      const lastHyphen = albumName.lastIndexOf('-');
+      normalizedName = albumName.slice(0, lastHyphen - 1);
+    } else {
+      // Descriptors are usually wrapped inside brackets, so we'll remove anything inside those brackets.
+      const lastBracket = albumName.lastIndexOf('(');
+      normalizedName = albumName.slice(0, lastBracket - 1);
+    }
 
-  if (descriptorIndex < 0) {
-    descriptorIndex = albumName.indexOf('Original Motion Picture Soundtrack');
-  }
-
-  // Descriptors are usually wrapped inside brackets, so we'll remove anything inside those brackets.
-  if (descriptorIndex > 0) {
-    const lastBracket = albumName.lastIndexOf('(');
-    const normalizedName = albumName.slice(0, lastBracket - 1);
     return normalizedName;
   }
 
